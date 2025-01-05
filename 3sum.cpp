1d4dca9f0bda34324fe95a4ecf1f0d3b819be7d8
Shiven Goomer <shivengoomer@gmail.com>
2025-01-05 15:19:27 +0530

    poweof3

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,32 +1,14 @@
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-     vector<vector<int>> temp;
-        int len = nums.size();
-        sort(nums.begin(), nums.end()); 
+#include <iostream>
+#include <algorithm> 
+#include<string>
+#include<vector>
+using namespace std;
 
-        for (int i = 0; i < len - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int left = i + 1;
-            int right = len - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    temp.push_back({nums[i], nums[left], nums[right]});
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1])                 right--;
-                    left++;
-                    right--;
-                } 
-                else if (sum < 0) {
-                    left++;
-                } 
-                else {
-                    right--;
-                }
-            }
-        }
-        return temp;
-    }
-};
+int main(){
+    int nums=123;
+    if()
+    
+    
+    
+}
+ 
---
+++ divide2nums.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm> 
+#include<string>
+#include<vector>
+using namespace std;
+int main(){
+    int divisor,dividend;
+    cin>>divisor>>dividend;
+    int temp=divisor;
+    while (temp>=divisor)
+    {
+        temp=temp=dividend;
+    }
+    cout<<temp;
+    
+    
+    
+}
+ 
