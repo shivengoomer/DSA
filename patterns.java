4c9c32522796e7bf63eae7334ca5911b7fadaea4
Shiven Goomer <shivengoomer@gmail.com>
2025-03-30 15:06:57 +0530

    Patterns

---
+++ patterns.java
@@ -0,0 +1,11 @@
+public class Patterns {
+    public static void alphaRamp(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                char m=(char)('A'+i-1);
+                System.out.print(m+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
